0e09c5563f7e40b57c625d4538928d46277fe978
shardul162 <shardul162@gmail.com>
2017-02-24 19:00:57 +0100

    Add files via upload

--- Master_Thesis/src/alpha.java
+++ Master_Thesis/src/alpha.java
@@ -2,22 +2,11 @@
 public class alpha {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Eye_Tracker summon = new Eye_Tracker();
-		
 		
 		Thread t1 = new Thread(new GUI());
-		Thread t2 = new Thread(new Eye_Tracker());
 		
-		//t2.start();
 		t1.start();
-		t2.start();
 		
-		//if(summon.Tstart==true){
-			//t2.start();
-			
-		//}
-
 	}
-
+	
 }
